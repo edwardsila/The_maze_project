a90c5cbbe321cb7d8a164f8674b86df0719be403
edwardsila <edwardsila000@gmail.com>
2024-05-30 13:33:26 -0700

    texture of everything updated

--- src/texture.c
+++ src/texture.c
@@ -1,14 +1,14 @@
-#include "../headers/header.h"
+#include "../include/header.h"
 
 static const char *textureFileNames[NUM_TEXTURES] = {
-	"./images/redbrick.png",
-	"./images/purplestone.png",
-	"./images/mossystone.png",
-	"./images/graystone.png",
-	"./images/colorstone.png",
-	"./images/bluestone.png",
-	"./images/wood.png",
-	"./images/eagle.png",
+	"./assets/redbrick.png",
+	"./assets/purplestone.png",
+	"./assets/mossystone.png",
+	"./assets/graystone.png",
+	"./assets/colorstone.png",
+	"./assets/bluestone.png",
+	"./assets/wood.png",
+	"./assets/eagle.png",
 };
 
 /**
